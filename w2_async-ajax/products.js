864e8f163ccdd6e170e4f8937b6878b90bc35a0c
Haojing XU <leslie.hj.xu.design@gmail.com>
2024-02-01 02:14:59 +0100

    first commit

---
+++ w2_async-ajax/products.js
@@ -0,0 +1,49 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+createApp({
+  data() {
+      return {
+          baseUrl: 'https://vue3-course-api.hexschool.io/v2',
+          apiPath: 'haojing',
+          products: [],
+          showProduct: {},
+      }
+  },
+  methods: {
+    checkUser() {
+      const api = `${this.baseUrl}/api/user/check`;
+      axios.post(api)
+        .then(api => {
+          this.getData();
+        })
+        .catch(err => {
+          console.log(err);
+          window.location = 'login.html';
+        })
+    },
+
+    getData() {
+      const api = `${this.baseUrl}/api/${this.apiPath}/admin/products/all`;
+      axios.get(api)
+        .then(res => {
+          this.products = res.data.products;
+        })
+    }
+  },
+  mounted() {
+    // 取得token
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)haojingToken\s*\=\s*([^;]*).*$)|^.*$/,
+      "$1",
+    );
+    console.log(token);
+    // 管理控制台的api必須帶入token，要帶入header裡
+    axios.defaults.headers.common['Authorization'] = token;
+
+    // this.getData();
+    this.checkUser();
+  }
+
+}).mount('#app');
+
+
